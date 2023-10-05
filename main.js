adb7dad7cdfa7f219b97bc05de7d4ccd343a181a
HImesh <himesh@raoinformationtechnology.com>
2023-10-05 22:24:05 +0530

    local notification

--- main.js
+++ main.js
@@ -7,7 +7,21 @@ if ('serviceWorker' in navigator) {
             console.error('Service Worker registration failed:', error);
         });
 }
-
+function persistentNotification() {
+    if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+      alert('Persistent Notification API not supported!');
+      return;
+    }
+    
+    try {
+      navigator.serviceWorker.getRegistration()
+        .then((reg) => reg.showNotification("Hi there - persistent!"))
+        .catch((err) => alert('Service Worker registration error: ' + err));
+    } catch (err) {
+      alert('Notification API error: ' + err);
+    }
+  }
+  
 document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('startButton');
     const stopButton = document.getElementById('stopButton');
